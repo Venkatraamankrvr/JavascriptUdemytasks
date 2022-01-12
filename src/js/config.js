02ab39fc418ee66ee1c7f6378a1feced53c6243d
Venkatraamankr <venkatraamankr17@gmail.com>
2022-01-13 02:59:25 +0530

    forkify-jspart10completed

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+
+export const TIMEOUT_SECOND = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7eaaca82-e87f-4a58-87ab-2ac755f8e83d';
+export const MODAL_CLOSE_SEC = 2.5;
